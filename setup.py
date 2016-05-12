03d1ffaa2c59bb9b858c08e7be18aedf91831e87
Xavier Barnada <xbarnada@gisce.net>
2016-05-12 11:16:44 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+import setuptools
+
+
+setuptools.setup(
+    name='cini',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/gisce/tipoinstalacion',
+    license='MIT',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    description="Libreria para calcular de forma automática el Tipo de"
+                " Instalacion (TI)"
+)
