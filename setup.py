d244a0df41879e495f630b6caee64a9c8bd3005e
Eduard Carreras <ecarreras@gisce.net>
2016-09-07 15:44:37 +0200

    Bump to v0.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='tipoinstalacion',
-    version='0.1.5',
+    version='0.1.6',
     packages=setuptools.find_packages(),
     url='https://github.com/gisce/tipoinstalacion',
     license='MIT',
