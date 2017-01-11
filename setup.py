301217de7810594d8c018b85eee07d6b4c574393
Xavier Barnada <xbarnada@gisce.net>
2017-01-11 09:31:34 +0100

    Bump to v0.1.8
    Force version to upload to pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='tipoinstalacion',
-    version='0.1.7',
+    version='0.1.8',
     packages=setuptools.find_packages(),
     url='https://github.com/gisce/tipoinstalacion',
     license='MIT',
