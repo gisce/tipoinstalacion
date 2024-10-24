52a53893ec712dd424e2deebe766c21ec4622b34
giscegit <devel@gisce.net>
2024-10-24 12:10:11 +0000

    Bump to v1.0.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name='tipoinstalacion',
-    version='0.1.9',
+    version='1.0.0',
     packages=setuptools.find_packages(),
     url='https://github.com/gisce/tipoinstalacion',
     license='MIT',
