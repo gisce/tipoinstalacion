4e634806f35068dd95233a0ac56f5a0a9c0e6c9f
PSala <polsalalidon@gmail.com>
2024-10-24 12:59:50 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setuptools.setup(
     url='https://github.com/gisce/tipoinstalacion',
     license='MIT',
     author='GISCE-TI, S.L.',
+    long_description='''ibreria para calcular de forma automática el Tipo de Instalacion (TI)''',
     author_email='devel@gisce.net',
     description="Libreria para calcular de forma automática el Tipo de"
                 " Instalacion (TI)"
